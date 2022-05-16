c896b676bac1ed7d68d1631b249891cef186e300
alejandrocora <alzapicolara@gmail.com>
2022-05-16 20:16:47 +0200

    removing trash

--- lib/selaux.py
+++ lib/selaux.py
@@ -6,7 +6,6 @@ from selenium.webdriver.firefox.options import Options
 def headlessChrome():
     options = Options()
     options.add_argument('--headless')
-    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
     driver  = webdriver.Chrome(options=options)
     return driver
 
