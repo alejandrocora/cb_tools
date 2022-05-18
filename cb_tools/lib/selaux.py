f0e1c54be150014eac33a3eef9b8573df82b0c3f
alejandrocora <alzapicolara@gmail.com>
2022-05-18 13:58:20 +0200

    fixed selaux

--- cb_tools/lib/selaux.py
+++ cb_tools/lib/selaux.py
@@ -1,16 +1,18 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as chrome_Options
+from selenium.webdriver.firefox.options import Options as firefox_Options
 
 def headless_chrome():
-    options = Options()
+    options = chrome_Options()
     options.add_argument('--headless')
+    options.add_argument("user-agent=Chrome/99.0.4844.51")
     driver  = webdriver.Chrome(options=options)
     return driver
 
 def headless_firefox():
-        options = Options()
+        options = firefox_Options()
         options.add_argument('--headless')
-        driver  = webdriver.Firefox(options=options, service_log_path=os.devnull)
+        options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
+        driver  = webdriver.Firefox(firefox_options=options, service_log_path=os.devnull)
         return driver
