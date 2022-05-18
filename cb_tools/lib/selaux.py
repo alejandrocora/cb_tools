9430d31d956dd0225c700ac281897aff6a839a82
alejandrocora <alzapicolara@gmail.com>
2022-05-18 16:31:42 +0200

    fixed selaux

--- cb_tools/lib/selaux.py
+++ cb_tools/lib/selaux.py
@@ -14,5 +14,5 @@ def headless_firefox():
         options = firefox_Options()
         options.add_argument('--headless')
         options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
-        driver  = webdriver.Firefox(firefox_Options=options, service_log_path=os.devnull)
+        driver  = webdriver.Firefox(options=options, service_log_path=os.devnull)
         return driver
